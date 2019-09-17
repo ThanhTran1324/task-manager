ab509918d8dfc7a63f8e51b0dcdcff4d250cf03f
Thanh Tran <tenchu1109@Thanhs-MacBook-Pro.local>
2019-09-17 10:59:45 -0400

    Updates

---
+++ precache-manifest.c81a314ac6fe81e274f00b91afbda699.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0511b66c2eecf964dc002442cee232",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "4a190341e5c040fae125",
+    "url": "/task-manager/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "70ed79575e39ba8d48da",
+    "url": "/task-manager/static/js/2.21f62a5b.chunk.js"
+  },
+  {
+    "revision": "4a190341e5c040fae125",
+    "url": "/task-manager/static/js/main.75528dca.chunk.js"
+  },
+  {
+    "revision": "7a00239df7f2bd5edc6b",
+    "url": "/task-manager/static/js/runtime~main.f472589c.js"
+  }
+]);
