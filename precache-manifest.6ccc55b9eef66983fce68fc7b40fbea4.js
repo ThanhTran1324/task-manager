05f1d8b6b8ab68cbf84a8293ab9644507849b900
thanhtran1324 <thanhtran1324@gmail.com>
2019-10-08 10:36:25 -0400

    Updates

---
+++ precache-manifest.6ccc55b9eef66983fce68fc7b40fbea4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6095ab4ee4b335e2c67ae686abe1f6c",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "f21353fad3ee94ac71ed",
+    "url": "/task-manager/static/css/main.f6f0ce22.chunk.css"
+  },
+  {
+    "revision": "2cecd1d31a69ccda8cd0",
+    "url": "/task-manager/static/js/2.d3c7313b.chunk.js"
+  },
+  {
+    "revision": "f21353fad3ee94ac71ed",
+    "url": "/task-manager/static/js/main.6ffa32bb.chunk.js"
+  },
+  {
+    "revision": "7a00239df7f2bd5edc6b",
+    "url": "/task-manager/static/js/runtime~main.f472589c.js"
+  }
+]);
